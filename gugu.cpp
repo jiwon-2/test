b727937d2120b305a1a52904588d6f5cf8ef3a5e
jiiwon-2 <wldnjs3433@naver.com>
2020-03-16 20:42:00 +0900

    init

---
+++ gugu.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1; i<10; i++)
+	{
+		j=0;
+		if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}
+		if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}
+		if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}
+		if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}
+		if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}if(j<9)
+		{
+			j++;
+			printf("%d * %d = %d",i,j,i*j);
+			printf("\n");
+		}
+	}
+}
